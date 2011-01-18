58272a620524430e7283198efb82492fb46a8bb6
robertp <robertp@robertpvm.localdomain>
2011-01-18 16:24:22 -0500

    Finished Scout's modifications to make it a touch harder.

--- scout/scout.c
+++ scout/scout.c
@@ -4,7 +4,8 @@
 #include <string.h>
 
 void doPrint(int c);
-void banner();
+
+char banner[] = "[ H A C K | F O R T R E S ]\n\n";
 
 int main(int argc, char** argv)
 {
@@ -13,7 +14,8 @@ int main(int argc, char** argv)
   int ciphered[] = { 24, 7, 21, 90, 13, 18, 13, 19, 2, 74, 76, 36, 51, 43 };
   int i = 0;
  
-  banner();
+  printf("%s", banner);
+
   for ( i; i < (sizeof(ciphered)/sizeof(int)); i++ )
   {
     ciphered[i] = ciphered[i] ^ key[i%4];
@@ -26,29 +28,28 @@ int main(int argc, char** argv)
 
 int lol(int argc, char** argv)
 {
-  if( argc < 16 )
+  if( argc < 16 || !( argv[1][0] == banner[2]+1 && argv[2][0] == banner[4]+1 ) )
   {
     main(argc, argv);
   }
-  banner(); 
+
   // Print "\t*Bonk!*\n\n"
-  doPrint(9);
-  doPrint(42);
-  doPrint(66);
-  doPrint(111);
-  doPrint(110);
-  doPrint(107);
-  doPrint(33);
-  doPrint(42);
-  doPrint(10);
-  doPrint(10);
+  printf("%s", banner);
+
+  unsigned int a = 0x8048690; // +0
+  unsigned int b = 0x8048699; // +9
+  unsigned int c = 0x804869A; // +10 / A
+  unsigned int d = 0x80486BA; // +42 / 2A
+  unsigned int e = 0x80486D2; // +66 / 42
+  unsigned int f = 0x80486FB; // +107 / 6B
+  unsigned int g = 0x80486FE; // +110 / 6E
+  unsigned int h = 0x80486FF; // +111 / 6F
+  unsigned int i = 0x80486B1; // +33 / 21
+
+  printf("%c%c%c%c%c%c%c%c%c%c",b-a,d-a,e-a,h-a,g-a,f-a,i-a,d-a,c-a,c-a);
   exit(0);
 }
 
-void banner()
-{
-  printf("[ H A C K | F O R T R E S ]\n\n");
-}
 
 void doPrint(int c)
 {
