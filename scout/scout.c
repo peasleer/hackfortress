eec8751d4be48c18bd7b17e720e5051029a00204
robertp <robertp@robertpvm.localdomain>
2011-01-21 12:17:39 -0500

    Fixed scout obfuscation, added description to puzzles.

--- scout/scout.c
+++ scout/scout.c
@@ -3,6 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
+// Macro taken from Chapter 12 of C/C++ Secure Programming
+#define DISASM_FALSERET asm volatile (               \
+            "  pushl %ecx        /* save registers    */\n" \
+            "  pushl %ebx                               \n" \
+            "  pushl %edx                               \n" \
+            "  movl  %esp, %ebx  /* save ebp, esp     */\n" \
+            "  movl  %ebp, %esp                         \n" \
+            "  popl  %ebp        /* save old %ebp     */\n" \
+            "  popl  %ecx        /* save return addr  */\n" \
+            "  lea   0f, %edx    /* edx = addr of 0:  */\n" \
+            "  pushl %edx        /* return addr = edx */\n" \
+            "  ret                                      \n" \
+            "  .byte 0x0F        /* off-by-one byte   */\n" \
+            "0:                                         \n" \
+            "  pushl %ecx        /* restore ret addr  */\n" \
+            "  pushl %ebp        /* restore old &ebp  */\n" \
+            "  movl  %esp, %ebp  /* restore ebp, esp  */\n" \
+            "  movl  %ebx, %esp                         \n" \
+            "  popl  %ebx                               \n" \
+            "  popl  %ecx                               \n")
+
 void doPrint(int c);
 
 char banner[] = "[ H A C K | F O R T R E S S ]\n\n";
@@ -27,7 +48,8 @@ int main(int argc, char** argv)
 
 int newMain(int argc, char** argv)
 {
-  
+ 
+  DISASM_FALSERET;
   if( argc < 16 || !( argv[1][0] == banner[2]+1 && argv[2][0] == banner[4]+1 ) )
   {
     //printf("argc: %i, argv[1][0]: %c, argv[2][0]: %c\n", argc, argv[1][0], argv[2][0]);
